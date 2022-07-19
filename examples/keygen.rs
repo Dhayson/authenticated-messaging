7e8bb1f6d358ba4623af493ae971a32ffdc2fd1d
Dhay <Diego50094@gmail.com>
2022-07-19 18:57:33 -0300

    Increase key size on keygen

--- examples/keygen.rs
+++ examples/keygen.rs
@@ -5,27 +5,27 @@ use std::os::unix::fs::OpenOptionsExt;
 fn main()
 {
     let mut rng = rand::thread_rng();
-    let bits = 512;
+    let bits = 2048;
     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
     let mut options = OpenOptions::new();
     let options = options.mode(0o600).write(true).create_new(true);
 
-    fs::remove_file("private.pem").ok();
-    fs::remove_file("public.pem").ok();
+    fs::remove_file("private4.pem").ok();
+    fs::remove_file("public4.pem").ok();
 
-    options.open("private.pem").unwrap();
-    options.open("public.pem").unwrap();
+    options.open("private4.pem").unwrap();
+    options.open("public4.pem").unwrap();
 
     fs::write(
-        "private.pem",
+        "private4.pem",
         pkcs1::EncodeRsaPrivateKey::to_pkcs1_pem(&priv_key, rsa::pkcs8::LineEnding::LF)
             .unwrap()
             .as_str(),
     )
     .ok();
     fs::write(
-        "public.pem",
+        "public4.pem",
         pkcs1::EncodeRsaPublicKey::to_pkcs1_pem(&pub_key, rsa::pkcs8::LineEnding::LF)
             .unwrap()
             .as_str(),
